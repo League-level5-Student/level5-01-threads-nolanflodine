4287797d832af32475c1a69d26710d4369d726ee
Floyo <Floyo@DESKTOP-G71OOBP.attlocal.net>
2020-09-02 17:31:22 -0700

    Olympic Rings

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,16 @@
 package _01_Olympic_Rings;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+public static void main(String[] args) {
+	Robot blue = new Robot("batman");
+	Robot black = new Robot("batman");
+	Robot red = new Robot("batman");
+	Robot yellow = new Robot("batman");
+	Robot green = new Robot("batman");
 
+}
 }
 
