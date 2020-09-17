cd2b572e357073578c2971dd39909e178aef5d91
Floyo <Floyo@DESKTOP-G71OOBP.attlocal.net>
2020-09-16 17:28:22 -0700

    ThreadPool, SynchronizedSwimming

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,31 @@
 package _04_Thread_Pool;
 
-public class ThreadPool {
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+public class ThreadPool{
+Thread[] threads;
+ConcurrentLinkedQueue<Task> taskQue;
+public ThreadPool(int totalThreads) {
+	threads = new Thread[totalThreads];
+	taskQue = new ConcurrentLinkedQueue<Task>();
+	for (int i = 0; i < threads.length; i++) {
+		threads[i] = new Thread(new Worker(taskQue));
+	}
+}
+public void addTask(Task task) {
+	taskQue.add(task);
+}
+public void Start() {
+	for (int i = 0; i < threads.length; i++) {
+		threads[i].start();
+	}
+	for (int i = 0; i < threads.length; i++) {
+		try {
+			threads[i].join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
 }
--- src/_04_Thread_Pool/Worker.java
+++ src/_04_Thread_Pool/Worker.java
@@ -1,12 +1,19 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class Worker implements Runnable {
-
+public class Worker implements Runnable{
+	ConcurrentLinkedQueue<Task> taskQue;
+	public Worker(ConcurrentLinkedQueue<Task> taskQue) {
+		this.taskQue = taskQue;
+	}
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		while(!taskQue.isEmpty()) {
+			Task t = taskQue.remove();
+			t.perform();
+		}
 	}
 
 
